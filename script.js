3441236e5562a6e15413808d487ad01e33b51588
Monika Woźniak <monikawozniak07@gmail.com>
2020-08-02 11:33:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,137 @@
+// noise texture
+const noise = () => {
+  let canvas, ctx;
+  let wWidth, wHeight;
+  let noiseData = [];
+  let frame = 0;
+  let loopTimeout;
+
+  // Create Noise
+  const createNoise = () => {
+    const idata = ctx.createImageData(wWidth, wHeight);
+    const buffer32 = new Uint32Array(idata.data.buffer);
+    const len = buffer32.length;
+
+    for (let i = 0; i < len; i++) {
+      if (Math.random() < 0.5) {
+        buffer32[i] = 0xff000000;
+      }
+    }
+
+    noiseData.push(idata);
+  };
+
+  // Play Noise
+  const paintNoise = () => {
+    if (frame === 9) {
+      frame = 0;
+    } else {
+      frame++;
+    }
+
+    ctx.putImageData(noiseData[frame], 0, 0);
+  };
+
+  // Loop
+  const loop = () => {
+    paintNoise(frame);
+
+    loopTimeout = window.setTimeout(() => {
+      window.requestAnimationFrame(loop);
+    }, 1000 / 25);
+  };
+
+  // Setup
+  const setup = () => {
+    wWidth = window.innerWidth;
+    wHeight = window.innerHeight;
+
+    canvas.width = wWidth;
+    canvas.height = wHeight;
+
+    for (let i = 0; i < 10; i++) {
+      createNoise();
+    }
+
+    loop();
+  };
+
+  // Reset
+  let resizeThrottle;
+  const reset = () => {
+    window.addEventListener(
+      "resize",
+      () => {
+        window.clearTimeout(resizeThrottle);
+
+        resizeThrottle = window.setTimeout(() => {
+          window.clearTimeout(loopTimeout);
+          setup();
+        }, 200);
+      },
+      false
+    );
+  };
+
+  // Init
+  const init = (() => {
+    canvas = document.getElementById("noise");
+    ctx = canvas.getContext("2d");
+
+    setup();
+  })();
+};
+
+noise();
+
+// slogan
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  this.el.innerHTML = '<span class="typewriter-bar">' + this.txt + "</span>";
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewriter");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML =
+    ".typewriter > .typewriter-bar { border-right: 0.07em solid #fff}";
+  document.body.appendChild(css);
+};
