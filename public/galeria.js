40ac5d40ad84d9c3f21ceee819e206c59258e5f5
Monika Woźniak <monikawozniak07@gmail.com>
2020-09-03 21:16:32 +0200

    bold links in gallery

--- public/galeria.js
+++ public/galeria.js
@@ -18,35 +18,60 @@ jQuery(function ($) {
 //Grupowanie
 //Żeby działało wystarczy dodać klasy do projektów tak jak w projekty.html
 function wnetrzaFun() {
-  var elemsWnetrza = document.getElementsByClassName('wnetrza');
-  var elemsArchitektura = document.getElementsByClassName('architektura');
+  var elemsWnetrza = document.getElementsByClassName("wnetrza");
+  var elemsArchitektura = document.getElementsByClassName("architektura");
   for (var i = 0; i < elemsWnetrza.length; i += 1) {
-    elemsWnetrza[i].style.display = 'block';
+    elemsWnetrza[i].style.display = "block";
   }
   for (var i = 0; i < elemsArchitektura.length; i += 1) {
-    elemsArchitektura[i].style.display = 'none';
+    elemsArchitektura[i].style.display = "none";
   }
   msnry.layout();
 }
 function architekturaFun() {
-  var elemsWnetrza = document.getElementsByClassName('wnetrza');
-  var elemsArchitektura = document.getElementsByClassName('architektura');
+  var elemsWnetrza = document.getElementsByClassName("wnetrza");
+  var elemsArchitektura = document.getElementsByClassName("architektura");
   for (var i = 0; i < elemsWnetrza.length; i += 1) {
-    elemsWnetrza[i].style.display = 'none';
+    elemsWnetrza[i].style.display = "none";
   }
   for (var i = 0; i < elemsArchitektura.length; i += 1) {
-    elemsArchitektura[i].style.display = 'block';
+    elemsArchitektura[i].style.display = "block";
   }
   msnry.layout();
 }
 function wszystkieFun() {
-  var elemsWnetrza = document.getElementsByClassName('wnetrza');
-  var elemsArchitektura = document.getElementsByClassName('architektura');
+  var elemsWnetrza = document.getElementsByClassName("wnetrza");
+  var elemsArchitektura = document.getElementsByClassName("architektura");
   for (var i = 0; i < elemsWnetrza.length; i += 1) {
-    elemsWnetrza[i].style.display = 'block';
+    elemsWnetrza[i].style.display = "block";
   }
   for (var i = 0; i < elemsArchitektura.length; i += 1) {
-    elemsArchitektura[i].style.display = 'block';
+    elemsArchitektura[i].style.display = "block";
   }
   msnry.layout();
 }
+
+// pogrubianie linków
+const allImages = document.querySelector(".wszystkie-links a:nth-of-type(1)");
+const archiImages = document.querySelector(".wszystkie-links a:nth-of-type(2)");
+const interiorsImages = document.querySelector(
+  ".wszystkie-links a:nth-of-type(3)"
+);
+
+archiImages.addEventListener("click", function () {
+  allImages.classList.remove("active");
+  interiorsImages.classList.remove("active");
+  archiImages.classList.add("active");
+});
+
+allImages.addEventListener("click", function () {
+  allImages.classList.add("active");
+  interiorsImages.classList.remove("active");
+  archiImages.classList.remove("active");
+});
+
+interiorsImages.addEventListener("click", function () {
+  interiorsImages.classList.add("active");
+  allImages.classList.remove("active");
+  archiImages.classList.remove("active");
+});
