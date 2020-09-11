e8cf40c23c25e89dd11d9029b8ec990807c7e8ee
Monika Woźniak <monikawozniak07@gmail.com>
2020-09-11 14:03:12 +0200

    linki

--- public/galeria.js
+++ public/galeria.js
@@ -11,9 +11,9 @@ imagesLoaded(grid, function () {
   });
 });
 
-const allImages = document.getElementById('btn-all');
-const archiImages = document.getElementById('btn-architektura');
-const interiorsImages = document.getElementById('btn-wnetrza');
+const allImages = document.getElementById("btn-all");
+const archiImages = document.getElementById("btn-architektura");
+const interiorsImages = document.getElementById("btn-wnetrza");
 
 allImages.addEventListener("click", function () {
   allImages.classList.add("active");
@@ -21,19 +21,17 @@ allImages.addEventListener("click", function () {
   archiImages.classList.remove("active");
 });
 
-archiImages.addEventListener("click", function () {
-  allImages.classList.remove("active");
-  interiorsImages.classList.remove("active");
-  archiImages.classList.add("active");
-});
+// archiImages.addEventListener("click", function () {
+//   allImages.classList.remove("active");
+//   interiorsImages.classList.remove("active");
+//   archiImages.classList.add("active");
+// });
 
 interiorsImages.addEventListener("click", function () {
   interiorsImages.classList.add("active");
   allImages.classList.remove("active");
   archiImages.classList.remove("active");
 });
-
-
 
 var starting_slice_point = 0;
 var number_of_elements = 20;
@@ -48,19 +46,22 @@ $(function () {
     .then((res) => res.json())
     .then((data) => (obj = data))
     .then(() => {
-
       obj.sort(function (a, b) {
         return parseFloat(a.kolejnosc) - parseFloat(b.kolejnosc);
       });
 
       //filter data -> architektura
-      architektura_all = obj.filter(element => element.architektura_wnetrza === "architektura")
+      architektura_all = obj.filter(
+        (element) => element.architektura_wnetrza === "architektura"
+      );
       console.log(architektura_all);
       //filter data -> wnetrza
-      wnetrza_all = obj.filter(element => element.architektura_wnetrza === "wnetrza");
+      wnetrza_all = obj.filter(
+        (element) => element.architektura_wnetrza === "wnetrza"
+      );
       console.log(wnetrza_all);
       //filter data -> all
-      all = obj.filter(element => element.architektura_wnetrza !== "")
+      all = obj.filter((element) => element.architektura_wnetrza !== "");
       console.log(all);
       //slice data
       all = obj.slice(starting_slice_point, number_of_elements);
@@ -68,21 +69,24 @@ $(function () {
 
       //display first 20
       all.forEach((element) => {
-        document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+        document.getElementById(
+          "grid"
+        ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
       });
 
       //click "Wszystkie"
-      $(document).on('click', '#btn-all', function () {
-
+      $(document).on("click", "#btn-all", function () {
         //clear DOM elements
-        document.getElementById("grid").innerHTML = '';
+        document.getElementById("grid").innerHTML = "";
 
         //create number of elements
         all = all.slice(0, number_of_elements);
         all.forEach((element) => {
-          document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+          document.getElementById(
+            "grid"
+          ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
           console.log("NUMBER_OF: " + number_of_elements);
@@ -90,15 +94,16 @@ $(function () {
       });
 
       //click "Architektura"
-      $(document).on('click', '#btn-architektura', function () {
-
+      $(document).on("click", "#btn-architektura", function () {
         //clear DOM elements
-        document.getElementById("grid").innerHTML = '';
+        document.getElementById("grid").innerHTML = "";
 
         //create number of elements
         architektura_all = architektura_all.slice(0, number_of_elements);
         architektura_all.forEach((element) => {
-          document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+          document.getElementById(
+            "grid"
+          ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
           console.log("NUMBER_OF: " + number_of_elements);
@@ -106,65 +111,81 @@ $(function () {
       });
 
       //click "Wnetrza"
-      $(document).on('click', '#btn-wnetrza', function () {
-
+      $(document).on("click", "#btn-wnetrza", function () {
         //clear DOM elements
-        document.getElementById("grid").innerHTML = '';
+        document.getElementById("grid").innerHTML = "";
 
         //create number of elements
         wnetrza_all = wnetrza_all.slice(0, number_of_elements);
         wnetrza_all.forEach((element) => {
-          document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+          document.getElementById(
+            "grid"
+          ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
           console.log("NUMBER_OF: " + number_of_elements);
         });
       });
 
-
       //button "Zobacz wiecej"
-      $(document).on('click', '#btn', function () {
+      $(document).on("click", "#btn", function () {
         //clear DOM elements
-        document.getElementById("grid").innerHTML = '';
+        document.getElementById("grid").innerHTML = "";
 
-        if (document.getElementById("btn-all").classList.contains('active')) {
-          all = obj.filter(element => element.architektura_wnetrza !== "").slice(0, number_of_elements + 20);
+        if (document.getElementById("btn-all").classList.contains("active")) {
+          all = obj
+            .filter((element) => element.architektura_wnetrza !== "")
+            .slice(0, number_of_elements + 20);
           all.forEach((element) => {
-            document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+            document.getElementById(
+              "grid"
+            ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
             console.log("NUMBER_OF: " + number_of_elements);
           });
 
-          console.log("ALL ROZMIAR" + all.length)
+          console.log("ALL ROZMIAR" + all.length);
           number_of_elements += 20;
           return 0;
-        }
-        else if (document.getElementById("btn-architektura").classList.contains('active')) {
-          architektura_all = architektura_all = obj.filter(element => element.architektura_wnetrza === "architektura").slice(0, number_of_elements + 20);
+        } else if (
+          document
+            .getElementById("btn-architektura")
+            .classList.contains("active")
+        ) {
+          architektura_all = architektura_all = obj
+            .filter(
+              (element) => element.architektura_wnetrza === "architektura"
+            )
+            .slice(0, number_of_elements + 20);
           architektura_all.forEach((element) => {
-            document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+            document.getElementById(
+              "grid"
+            ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
           });
           console.log("ARCHITEKTURA ROZMIAR" + all.length);
           number_of_elements += 20;
           return 0;
-        }
-        else if (document.getElementById("btn-wnetrza").classList.contains('active')) {
-
-          wnetrza_all = obj.filter(element => element.architektura_wnetrza === "wnetrza").slice(0, number_of_elements + 20);
+        } else if (
+          document.getElementById("btn-wnetrza").classList.contains("active")
+        ) {
+          wnetrza_all = obj
+            .filter((element) => element.architektura_wnetrza === "wnetrza")
+            .slice(0, number_of_elements + 20);
           wnetrza_all.forEach((element) => {
-            document.getElementById("grid").innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
+            document.getElementById(
+              "grid"
+            ).innerHTML += `<div class="grid-item ${element.architektura_wnetrza}" onclick="document.location='../public/projekty/${element.nazwa_projektu}.html';return false;">
         <img src="${element.zdjecie_glowne}" alt="${element.nazwa}" />
       </div>`;
           });
-          console.log("WNETRZA ROZMIAR" + all.length)
+          console.log("WNETRZA ROZMIAR" + all.length);
           number_of_elements += 20;
           return 0;
         }
       });
-
     });
 });
 
@@ -211,5 +232,3 @@ function wszystkieFun() {
   }
   msnry.layout();
 }
-
-
