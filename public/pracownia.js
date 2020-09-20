49e70b495508532d3db4288bdaac0e3c1ece1198
Monika Woźniak <monikawozniak07@gmail.com>
2020-09-20 11:15:39 +0200

    vanta back

--- public/pracownia.js
+++ public/pracownia.js
@@ -14,3 +14,19 @@ function readmoref() {
     setReadMoreStyle(readmoreText, "inline");
   }
 }
+// V1
+VANTA.FOG({
+  el: "#vanta",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  highlightColor: 0x464646,
+  midtoneColor: 0x575757,
+  lowlightColor: 0x222222,
+  baseColor: 0x252525,
+  blurFactor: 0.15,
+  speed: 1.1,
+  zoom: 2.0,
+});
