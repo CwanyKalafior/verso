a64834329291ccf17f68656a6f2eaf344ea78395
Misiek <prehendi@gmail.com>
2020-09-25 14:05:33 +0200

    Fixes

--- public/pracownia.js
+++ public/pracownia.js
@@ -43,3 +43,22 @@ TweenMax.to({}, 0.016, {
     });
   },
 });
+
+// PosnAwr Button (readmore)
+
+$(function() {  
+  $('.pracownia-btn-posnawr')
+    .on('mouseenter', function(e) {
+            var parentOffset = $(this).offset(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+            var parentOffset = $(this).offset(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+    });
+  $('[href=#]').click(function(){return false});
+});
